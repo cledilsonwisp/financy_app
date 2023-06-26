561e7abe003e40be91549eef4cc7abce4bbb3fb6
Cledilson Silva <cavbrcleg@gmail.com>
2023-06-26 12:42:50 -0300

    Alterado fonts do big text para google fonts inter, criado test do onbording

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
   AppTextStyles._();
 
-  static const TextStyle bigText =
-      TextStyle(fontSize: 50, fontWeight: FontWeight.w700);
+  static TextStyle bigText =
+      GoogleFonts.inter(fontSize: 50, fontWeight: FontWeight.w700);
 }
