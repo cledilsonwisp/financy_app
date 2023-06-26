f33400f786e6f25e4813131b65a20acc199c7b84
Cledilson Silva <cavbrcleg@gmail.com>
2023-06-26 12:36:44 -0300

    Criado modulo Core e classes para textStyle e Colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color greenLightOne = Color(0XFF63B5AF);
+  static const Color greenLightTwo = Color(0XFF438883);
+  static const Color white = Color(0XFFFFFFFF);
+}
