da46111e3a4318e64a4d873740cb53142a8994e6
Cledilson Silva <cavbrcleg@gmail.com>
2023-06-29 16:11:07 -0300

    refactor do tamanho dos textos no AppTextStyles

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -1,13 +1,14 @@
+import 'package:financy_app/core/utils/dinamic_size_responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
   AppTextStyles._();
 
-  static TextStyle bigText =
-      GoogleFonts.inter(fontSize: 50, fontWeight: FontWeight.w700);
-  static TextStyle mediumText =
-      GoogleFonts.inter(fontSize: 22, fontWeight: FontWeight.bold);
-  static TextStyle smallText =
-      GoogleFonts.inter(fontSize: 10, fontWeight: FontWeight.w300);
+  static TextStyle bigText(BuildContext context) =>
+      GoogleFonts.inter(fontSize: DinamicSizeResponsive.of(context).dynamicScaleSize(size: 50), fontWeight: FontWeight.w700);
+  static TextStyle mediumText(BuildContext context) =>
+      GoogleFonts.inter(fontSize: DinamicSizeResponsive.of(context).dynamicScaleSize(size: 36) , fontWeight: FontWeight.w700);
+  static TextStyle smallText(BuildContext context) =>
+      GoogleFonts.inter(fontSize: DinamicSizeResponsive.of(context).dynamicScaleSize(size: 15), fontWeight: FontWeight.w300);
 }
