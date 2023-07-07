efd59d3637759d5385f27a4fed385b11b61138db
Cledilson Silva <cavbrcleg@gmail.com>
2023-07-07 14:00:35 -0300

    Adicionado nova color Grey

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -6,6 +6,7 @@ class AppColors {
   static const Color greenLightOne = Color(0XFF63B5AF);
   static const Color greenLightTwo = Color(0XFF438883);
   static const Color white = Color(0XFFFFFFFF);
+  static const Color grey = Color(0xFFB5B5B5);
 
   static List<Color> greenGradient = [greenLightOne, greenLightTwo];
   static List<Color> greyGradient = const [
