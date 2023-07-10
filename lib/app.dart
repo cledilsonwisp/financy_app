59d9309a5c66496e541036c289b9a768fdb23322
Cledilson Silva <cavbrcleg@gmail.com>
2023-07-10 15:16:55 -0300

    configurado screen util no material.

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,18 @@
 import 'package:financy_app/core/constants/app_themes.dart';
 import 'package:financy_app/features/sign_up/sign_up_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(
+      context,
+      designSize: const Size(375, 812), // Tamanho base do design
+      scaleByHeight: true,
+    );
     return MaterialApp(
       theme: AppTheme.defaultTheme(),
       debugShowCheckedModeBanner: false,
