495d42ad3365022284e27b3df0e80289b3502de1
Cledilson Silva <cavbrcleg@gmail.com>
2023-07-17 14:50:33 -0300

    Renomeado as cores,

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static const Color greenLightOne = Color(0XFF63B5AF);
-  static const Color greenLightTwo = Color(0XFF438883);
+  static const Color greenOne = Color(0XFF63B5AF);
+  static const Color greenTwo = Color(0XFF438883);
   static const Color white = Color(0XFFFFFFFF);
   static const Color grey = Color(0xFFB5B5B5);
   static const Color darkGrey = Color(0xFF666666);
 
-  static List<Color> greenGradient = [greenLightOne, greenLightTwo];
+  static List<Color> greenGradient = [greenOne, greenTwo];
   static List<Color> greyGradient = const [
     Color(0xFFB5B5B5),
     Color(0xFF7F7F7F)
