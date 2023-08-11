a9b91fe84518b0101fb2f56fe49971bfb6c0e16e
Cledilson Silva <cavbrcleg@gmail.com>
2023-08-11 15:14:43 -0300

    criado custom bottom sheet widget

--- lib/features/sign_up/presenter/sign_up_page.dart
+++ lib/features/sign_up/presenter/sign_up_page.dart
@@ -49,14 +49,37 @@ class _SignUpPageState extends State<SignUpPage> {
             ));
       }
       if (controller.state is SignUpErrorState) {
-        showDialog(
-            context: context,
-            builder: (context) => const Scaffold(
-                  body: SizedBox(
-                    height: 150,
-                    child: Text('Erro ao logar, tente novamente'),
-                  ),
-                ));
+        showModalBottomSheet<void>(
+          context: context,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          builder: (BuildContext context) {
+            return SizedBox(
+              height: 250,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Text('Ops, Algo deu errado'),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 32),
+                      child: PrimaryButton(
+                          title: 'Tentar Novamento',
+                          onTap: () => Navigator.pop(context)),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
       }
     });
     super.initState();
